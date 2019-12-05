60a05f66f498a5e5aeabc92a33bf76eed0b2e803
Samuel <samuelhereira@gmail.com>
2019-12-05 14:04:24 -0500

    Se agregaron las validaciones respectivas al ingreso de cedula, numero de telefono, correo, horas faltas y en las opciones multiples

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,32 @@
 import java.util.Scanner;
 
 public class Main {
+    /** Para VALIDAR CEDULA **/
+    static boolean validacion(String cedula) {
 
+        int sumaimpar = 0, sumapar = 0, digito = 0, digitofinal = 0, resultado = 0, digitovalidacion = 0;
+
+        for(int i = 0; i < 9; i++) {
+            if(i % 2 == 0) {
+                digito = Character.getNumericValue(cedula.charAt(i));
+                digitofinal = digito*2;
+                if(digitofinal>9) {
+                    digitofinal -= 9;
+                }
+                sumaimpar += digitofinal;
+            }
+            else {
+                sumapar += Character.getNumericValue(cedula.charAt(i));
+            }
+
+        }
+        resultado = sumaimpar + sumapar;
+
+        digitovalidacion = resultado % 10;
+        return (digitovalidacion + Character.getNumericValue(cedula.charAt(9)) == 10);
+    }
+
+    /**Para INGRESAR DATOS DEL DOCENTE **/
     public static void ingresarDatosDocente(Docente docente) {
         String nombre, apellido, cedula, direccion, correo, telefono;
         int modalidad, tipoCarga, horasFalta;
@@ -15,57 +40,76 @@ public class Main {
         apellido = ingresar.nextLine();
         docente.setApellidos(apellido);
 
-        System.out.println("Ingrese la cedula del docente");
-        cedula = ingresar.nextLine(); // VALIDAR CEDULA
+        do {
+            System.out.println("Ingrese la cedula del docente");
+            cedula = ingresar.nextLine(); // VALIDAR CEDULA
+            if(!validacion(cedula)) {
+                System.out.println("Ingrese una cedula valida");
+            }
+        } while(!validacion(cedula));
         docente.setCedula(cedula);
 
         System.out.println("Ingrese la direccion del docente");
         direccion = ingresar.nextLine();
         docente.setDireccion(direccion);
 
-        System.out.println("Ingrese el correo del docente");
-        correo = ingresar.nextLine(); // VALIDAR QUE CONTENGA @
+        do {
+            System.out.println("Ingrese el correo del docente");
+            correo = ingresar.nextLine(); // VALIDAR QUE CONTENGA @
+            if(!correo.contains("@")) {
+                System.out.println("Ingrese un correo valido");
+            }
+        } while(!correo.contains("@"));
         docente.setCorreo(correo);
 
-        System.out.println("Ingrese el numero de telefono del docente");
-        telefono = ingresar.nextLine(); // VALIDAR QUE SEA DE 10 DIGITOS
+        do {
+            System.out.println("Ingrese el numero de telefono del docente");
+            telefono = ingresar.nextLine();
+        } while(telefono.length() != 10 || !telefono.matches("[0-9]+")); // VALIDAR QUE SEA DE 10 DIGITOS
         docente.setTelefono(telefono);
 
-        System.out.println("Modalidad de contrato del docente: \n1. Contrato \t 2. Nombramiento");
-        modalidad = ingresar.nextInt();
-        if(modalidad == 1) {
+        do {
+            System.out.println("Modalidad de contrato del docente: \n1. Contrato \t 2. Nombramiento");
+            modalidad = ingresar.nextInt();
+        } while(modalidad != 1 && modalidad != 2);
+        if (modalidad == 1) {
             docente.setModalidad(new Contrato());
         } else if (modalidad == 2) {
             int tipo;
             docente.setModalidad(new Nombramiento());
-            System.out.println("Tipo de nombramiento del docente \n1. Auxiliar \t 2. Agregado \t 3. Pricipal");
-            tipo = ingresar.nextInt();
-            if(docente.getModalidad() instanceof Nombramiento) {
-                if (tipo == 1) {
-                    ((Nombramiento) docente.getModalidad()).setTipoNombramiento(TipoNombramiento.AUXILIAR);
-                } else if (tipo == 2) {
-                    ((Nombramiento) docente.getModalidad()).setTipoNombramiento(TipoNombramiento.AGREGADO);
-                } else if (tipo == 3) {
-                    ((Nombramiento) docente.getModalidad()).setTipoNombramiento(TipoNombramiento.PRINCIPAL);
-                }
+            do {
+                System.out.println("Tipo de nombramiento del docente \n1. Auxiliar \t 2. Agregado \t 3. Pricipal");
+                tipo = ingresar.nextInt();
+                if (docente.getModalidad() instanceof Nombramiento) {
+                    if (tipo == 1) {
+                        ((Nombramiento) docente.getModalidad()).setTipoNombramiento(TipoNombramiento.AUXILIAR);
+                    } else if (tipo == 2) {
+                        ((Nombramiento) docente.getModalidad()).setTipoNombramiento(TipoNombramiento.AGREGADO);
+                    } else if (tipo == 3) {
+                        ((Nombramiento) docente.getModalidad()).setTipoNombramiento(TipoNombramiento.PRINCIPAL);
+                    }
 
-            }
-        } else {
-            /*** VALIDAR **/
+                }
+            } while(tipo != 1 && tipo != 2 && tipo != 3);
         }
 
-        System.out.println("Tipo de carga del docente \n1. Medio Tiempo \t 2. Tiempo Completo");
-        tipoCarga = ingresar.nextInt();
-        if(tipoCarga == 1) {
+        do {
+            System.out.println("Tipo de carga del docente \n1. Medio Tiempo \t 2. Tiempo Completo");
+            tipoCarga = ingresar.nextInt();
+        } while(tipoCarga !=1 && tipoCarga != 2);
+        if (tipoCarga == 1) {
             docente.getModalidad().setTipoCarga(TipoCarga.MEDIO_TIEMPO);
-        } else if(tipoCarga == 2) {
+        } else if (tipoCarga == 2) {
             docente.getModalidad().setTipoCarga(TipoCarga.TIEMPO_COMPLETO);
         } else {
             /** VALIDAR **/
         }
 
+
+        do {
         System.out.println("Ingrese el numero de horas faltadas");
         horasFalta = ingresar.nextInt();
+        } while(horasFalta < 0);
         docente.setHorasFaltas(horasFalta);
 
         docente.setHorasTrabajo(30);
